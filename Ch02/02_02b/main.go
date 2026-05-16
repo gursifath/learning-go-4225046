f6a5c173adfad14cbf0c27c3d2d0a1fdd6a15957
Gursifath Bhasin <gursifath@users.noreply.github.com>
2026-05-16 15:59:18 +0000

    Chapter 2: fmt outputs

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,19 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	aNumber := 42
 
 	fmt.Println("Hello from Go!")
+	fmt.Println(str1, str2, str3)
+	strLength, err := fmt.Println("The number is", aNumber)
+	if err == nil {
+		fmt.Println("The length of string is", strLength)
+	}
+
+	fmt.Printf("The value is %v\n", aNumber)
+	fmt.Printf("The type is %T\n", aNumber)
+	fmt.Printf("The type is %T\n%T\n%T\n", str1, str2, str3)
 }
