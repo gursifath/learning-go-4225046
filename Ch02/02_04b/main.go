03605a572023ebabcb8783f94ed7f067b5b1410f
Gursifath Bhasin <gursifath@users.noreply.github.com>
2026-05-16 18:21:43 +0000

    using math operators

--- Ch02/02_04b/main.go
+++ Ch02/02_04b/main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Math")
+
+	i1, i2, i3 := 1, 2, 3
+	intSum := i1 + i2 + i3
+	fmt.Println("Integer sum is ", intSum)
+
+	f1, f2, f3 := 4.55, 7.99, 2.87
+	floatSum := f1 + f2 + f3
+	fmt.Println("Float Sum is ", floatSum)
+
+	// total := i1 + f3 // invalid arithmetic operation between mismatching types
+
+	total := float64(i1) + f3
+	fmt.Println(total)
 }
