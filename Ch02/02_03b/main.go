0029d113fbfe9990f24b62baebd0d62f2503cf65
Gursifath Bhasin <gursifath@users.noreply.github.com>
2026-05-16 17:33:32 +0000

    reading inputs

--- Ch02/02_03b/main.go
+++ Ch02/02_03b/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter Text ")
+	str, _ := reader.ReadString('\n')
+	fmt.Println(str)
+
+	fmt.Print("Enter a Number ")
+	str, _ = reader.ReadString('\n')
+	f, err := strconv.ParseFloat(strings.TrimSpace(str), 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Value of str ", f)
+	}
 }
