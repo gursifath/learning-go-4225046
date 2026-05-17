c94deffdcc832d56632f09bb57faf41f8db95e2c
Gursifath Bhasin <gursifath@users.noreply.github.com>
2026-05-17 16:09:34 +0000

    read response from web

--- Ch06/06_02b/main.go
+++ Ch06/06_02b/main.go
@@ -2,16 +2,36 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
 	fmt.Println("Network requests")
+
+	client := http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	checkError(err)
+
+	req.Header.Set("User-Agent", "")
+
+	resp, err := client.Do(req)
+	checkError(err)
+	defer resp.Body.Close()
+
+	fmt.Printf("Type of Response is %T\n", resp)
+
+	bytes, err := io.ReadAll(resp.Body)
+	checkError(err)
+
+	content := string(bytes)
+	fmt.Println(content)
 }
 
-// func checkError(err error) {
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
