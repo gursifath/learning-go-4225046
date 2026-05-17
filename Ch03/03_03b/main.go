d585e700d2bb82cf4c65bf2b9a176e9c43a95cab
Gursifath Bhasin <gursifath@users.noreply.github.com>
2026-05-17 12:22:40 +0000

    Arrays

--- Ch03/03_03b/main.go
+++ Ch03/03_03b/main.go
@@ -6,4 +6,18 @@ import (
 
 func main() {
 	fmt.Println("Arrays")
+
+	var colors [3]string
+	colors[0] = "Red"
+	colors[1] = "Blue"
+	colors[2] = "Green"
+
+	fmt.Println(colors)
+
+	var numbers = [5]int{4, 6, 8, 0, 4}
+	fmt.Println(numbers)
+	fmt.Println(numbers[4])
+
+	fmt.Printf("Length of colors is %d\n", len(colors))
+	fmt.Printf("Length of numbers is %d\n", len(numbers))
 }
