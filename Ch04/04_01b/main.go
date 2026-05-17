5239166228a23ef06937d6b625851f6a36dc508e
Gursifath Bhasin <gursifath@users.noreply.github.com>
2026-05-17 14:46:45 +0000

    conditionals

--- Ch04/04_01b/main.go
+++ Ch04/04_01b/main.go
@@ -6,4 +6,16 @@ import (
 
 func main() {
 	fmt.Println("Conditional logic")
+
+	// someInt := 42 (can also directly define variable in if condition)
+	var result string
+
+	if someInt := 42; someInt < 0 {
+		result = "lesser than"
+	} else if someInt > 0 {
+		result = "greater than"
+	} else {
+		result = "equal to"
+	}
+	fmt.Println(result)
 }
