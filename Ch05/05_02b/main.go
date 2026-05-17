470a6b74aae9c49354e97c74236b7707c5cadd79
Gursifath Bhasin <gursifath@users.noreply.github.com>
2026-05-17 15:33:47 +0000

    methods

--- Ch05/05_02b/main.go
+++ Ch05/05_02b/main.go
@@ -6,10 +6,22 @@ import (
 
 func main() {
 	dog := Dog{"Poodle", "Woof"}
-	fmt.Printf("The %v says %v!\n", dog.Breed, dog.Sound)
+	dog.Speak()
+	dog.Sound = "Arf"
+	dog.Speak()
+	fmt.Println(dog.SpeakThreeTimes())
 }
 
 type Dog struct {
-   Breed string
-   Sound string
+	Breed string
+	Sound string
+}
+
+// in Go, a method is a member of a type
+func (d Dog) Speak() {
+	fmt.Printf("The %v says %v!\n", d.Breed, d.Sound)
+}
+
+func (d Dog) SpeakThreeTimes() string {
+	return fmt.Sprintf("%v! %v! %v!\n", d.Sound, d.Sound, d.Sound)
 }
