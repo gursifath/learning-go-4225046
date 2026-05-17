081a7b56559561ac2cac0e64c6334654f06e6efc
Gursifath Bhasin <gursifath@users.noreply.github.com>
2026-05-17 15:39:53 +0000

    goroutines

--- Ch05/05_03b/main.go
+++ Ch05/05_03b/main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
+	// turn any function to a goroutine by prepending go keyword
+	go say("hello from Amsterdam!!!")
 	fmt.Println("Goroutines")
+
+	// anonymous go routines
+	go func(message string) {
+		fmt.Println(message)
+	}("Hello from the Anonymous function")
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("All Done!!")
+}
+
+func say(message string) {
+	time.Sleep(1 * time.Second)
+	fmt.Println(message)
 }
