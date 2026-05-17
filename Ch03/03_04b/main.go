2cf32eda3a3315fab75ecbd59becf4e408588702
Gursifath Bhasin <gursifath@users.noreply.github.com>
2026-05-17 13:40:34 +0000

    slices

--- Ch03/03_04b/main.go
+++ Ch03/03_04b/main.go
@@ -2,10 +2,35 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
 	// This is an array
 	var colors = [3]string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
+
+	// This is a slice (it doesn't have a number or size within square brackets)
+	var colorsSlice = []string{"Red", "Green", "Blue"}
+	fmt.Println(colorsSlice)
+
+	// more common and memory efficient way to declare slice
+	var colorsSliceBetter = make([]string, 0, 4)
+	// 0 is the number of initial items in the slice
+	// 3 is the capacity of the slice
+
+	fmt.Println(colorsSliceBetter)
+
+	colorsSliceBetter = append(colorsSliceBetter, "Pink", "Orange", "Black", "Maroon")
+	fmt.Println(colorsSliceBetter)
+
+	colorsSliceBetter = remove(colorsSliceBetter, 1)
+	fmt.Println(colorsSliceBetter)
+
+	sort.Strings(colorsSliceBetter)
+	fmt.Println(colorsSliceBetter)
+}
+
+func remove(slice []string, i int) []string {
+	return append(slice[:i], slice[i+1:]...)
 }
