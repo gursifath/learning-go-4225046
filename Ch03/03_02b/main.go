2664d0ba17d0b8696e45a72e203d9b3b48c84d8b
Gursifath Bhasin <gursifath@users.noreply.github.com>
2026-05-17 12:17:14 +0000

    chap 3 - pointers

--- Ch03/03_02b/main.go
+++ Ch03/03_02b/main.go
@@ -6,4 +6,23 @@ import (
 
 func main() {
 	fmt.Println("Pointers")
+	// use * to refer to the value of the pointer
+	// use & to point to the memory address 
+
+	anyInt := 42
+	var p *int = &anyInt
+
+	if p == nil {
+		fmt.Println("p is nil")
+	} else {
+		fmt.Println("Value of p is", *p)
+	}
+
+	anyFloat := 42.13
+	pointer1 := &anyFloat
+
+	fmt.Println("Value of pointer1 is", *pointer1)
+	*pointer1 = *pointer1 / 13
+	fmt.Println("New value is", *pointer1)
+	fmt.Println("Original value is", anyFloat)
 }
