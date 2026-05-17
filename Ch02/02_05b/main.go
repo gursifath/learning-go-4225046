5842d23ffba8fd7a2dddadfa3e7a46b15224fb3c
Gursifath Bhasin <gursifath@users.noreply.github.com>
2026-05-17 09:58:26 +0000

    working with math package

--- Ch02/02_05b/main.go
+++ Ch02/02_05b/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 
 	f1, f2, f3 := 23.5, 65.1, 76.3
 	sum := f1 + f2 + f3
-	fmt.Println("Float sum:", sum)
+	result := math.Round(sum*100) / 100
+	fmt.Printf("Float sum: %.5f\n", result)
 
+	fmt.Println("Value of Pi is ", math.Pi)
+
+	circleRadius := 13.5
+	circumference := 2 * circleRadius * math.Pi
+
+	fmt.Printf("The circumference is %.4f\n", circumference)
 }
