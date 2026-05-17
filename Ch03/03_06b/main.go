5cdbee41438978fa49a9dcfad1197930b5c451aa
Gursifath Bhasin <gursifath@users.noreply.github.com>
2026-05-17 14:29:57 +0000

    struct

--- Ch03/03_06b/main.go
+++ Ch03/03_06b/main.go
@@ -6,4 +6,18 @@ import (
 
 func main() {
 	fmt.Println("Structs")
+
+	poodle := Dog{"Poodle", 34}
+	fmt.Println(poodle)
+	fmt.Printf("%+v\n", poodle)
+
+	poodle.Weight = 67
+	fmt.Println(poodle.Weight)
+	fmt.Println(poodle)
+}
+
+// capital starting letter signifies the function/field/variable/type as PUBLIC
+type Dog struct {
+	Breed  string
+	Weight int
 }
