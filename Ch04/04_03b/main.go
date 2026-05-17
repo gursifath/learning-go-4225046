00842a8a0c47110b464c003410a84e3d26a0fe9a
Gursifath Bhasin <gursifath@users.noreply.github.com>
2026-05-17 15:03:33 +0000

    for loops

--- Ch04/04_03b/main.go
+++ Ch04/04_03b/main.go
@@ -4,30 +4,30 @@ import "fmt"
 
 func main() {
 
-	// colors := []string{"Red", "Green", "Blue"}
-	// for i := 0; i < len(colors); i++ {
-	// 	println(colors[i])
-	// }
+	colors := []string{"Red", "Green", "Blue"}
+	for i := 0; i < len(colors); i++ {
+		println(colors[i])
+	}
 
-	// for i := range colors {
-	// 	println(colors[i])
-	// }
+	for i := range colors {
+		println(colors[i])
+	}
 
-	// for _, color := range colors {
-	// 	println(color)
-	// }
+	for _, color := range colors {
+		println(color)
+	}
 
-	// states := make(map[string]string)
-	// states["WA"] = "Washington"
-	// states["OR"] = "Oregon"
-	// states["CA"] = "California"
-	// for state, _ := range states {
-	// 	println(states[state])
-	// }
+	states := make(map[string]string)
+	states["WA"] = "Washington"
+	states["OR"] = "Oregon"
+	states["CA"] = "California"
+	for state, _ := range states {
+		println(states[state])
+	}
 
 	value := 0
 	sum := 0
-	for value < 5 {
+	for value < 5 { // used instead of while loop
 		sum += value
 		fmt.Printf("Value: %v\n", value)
 		fmt.Printf("Sum: %v\n", sum)
