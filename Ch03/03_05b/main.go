12024e22b29777055e6686f401f4d5962dda0d7e
Gursifath Bhasin <gursifath@users.noreply.github.com>
2026-05-17 14:14:04 +0000

    unordered maps

--- Ch03/03_05b/main.go
+++ Ch03/03_05b/main.go
@@ -2,8 +2,47 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
 	fmt.Println("Maps")
+
+	// read about make vs new functions
+	// new declares the variables but doesn't allocate memory
+	// make() allocates memory to the newly created variable
+	states := make(map[string]string)
+	states["UP"] = "Lucknow"
+	states["Tamil Nadu"] = "Chennai"
+	states["Maharashtra"] = "Mumbai"
+	fmt.Println(states)
+
+	fmt.Println(states["UP"])
+	fmt.Println(states["Maharashtra"])
+	up := states["UP"]
+	fmt.Println(up)
+
+	delete(states, "UP")
+	fmt.Println(states)
+
+	states["Goa"] = "Panaji"
+	fmt.Println(states)
+
+	// loop through map
+	for k, v := range states {
+		fmt.Printf("%s: %s\n", k, v)
+	}
+
+	keys := make([]string, len(states))
+	i := 0
+	for k := range states {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+	fmt.Println("Sorted")
+
+	for i := range keys {
+		fmt.Println(keys[i], states[keys[i]])
+	}
 }
